f7796a34d61d19154fef049c60fd9ba1d0924fd0
CoDan123 <let2350@gmail.com>
2022-03-10 20:39:49 -0700

    Added todays temp section

--- src/App.js
+++ src/App.js
@@ -40,43 +40,63 @@ function App() {
   }
 
   return (
-    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 60) ? 'app-warm' : 'app') : 'app'}>
+    <div className='app'>
       <main> 
-        <div className="search-container">
-          <div className='search-box'>
-            <input 
-              type="text" 
-              className='search-bar'
-              placeholder='Search...'
-              onChange={e => setQuery(e.target.value)}
-              value={query}
-              onKeyPress={search}
-            />
-          </div>
-        </div>
-        
-          {(typeof weather.main != "undefined") ? (
-            <div className="main-temp-container">
-                <div className="today-temp">
-                    <div className="temp-icon-container">
-                        <img src={`./images/${weather.weather[0].main}.svg`} alt='Weather Icon'></img>
-                    </div>
-                    <div className="temp-container">
-                        
-                    </div>
-                    <div className="location-box">
+
+          <div className="search-container">
+
+              {(typeof weather.main != "undefined") ? (
+                  <div className="location-date-box">
                       <div className="location">{weather.name}, {weather.sys.country}</div>
                       <div className="date">{dateBuilder(new Date())}</div>
-                    </div>
-                    <div className="weather-box">
-                      <div className="temp">{Math.round(weather.main.temp)}<FontAwesomeIcon className="degree-icon" icon={faO} /></div>
-                      <div className="weather">{weather.weather[0].description}</div>
-                    </div>
-                </div>
-                <div className="week-temp">
+                  </div>
+              ) : ('')}
+              
+              <div className='search-box'>
+                  <input 
+                    type="text" 
+                    className='search-bar'
+                    placeholder='Search...'
+                    onChange={e => setQuery(e.target.value)}
+                    value={query}
+                    onKeyPress={search}
+                  />
+              </div>
 
-                </div>
-            </div>
+          </div>
+        
+          {(typeof weather.main != "undefined") ? (
+
+              <div className="main-temp-container">
+
+                  <div className="today-temp">
+
+                      <div className="today-temp-left-box">
+                          <div className="temp-icon-container">
+                              <img src={`./images/${weather.weather[0].main}.svg`} alt='Weather Icon'></img>
+                          </div>
+            
+                          <div className="weather-box">
+                            <div className="temp">{Math.round(weather.main.temp)}<FontAwesomeIcon className="degree-icon" icon={faO} /></div>
+                            <div className="weather">{weather.weather[0].description}</div>
+                          </div>
+                      </div>
+
+                      <div className="today-temp-right-box">
+                        <div className="weather-data-top">
+
+                        </div>
+                        <div className="weather-data-bottom">
+                        </div>
+                      </div>
+
+                  </div>
+
+                  <div className="week-temp">
+                  </div>
+
+              </div>
+
           ) : ('')}
           
       </main>
