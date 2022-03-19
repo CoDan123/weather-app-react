0cfd307e08a079a73b0dc00c40e909642b27570c
CoDan123 <let2350@gmail.com>
2022-03-19 00:29:52 -0600

    Net functions update

--- Functions/fetchWeather.js
+++ Functions/fetchWeather.js
@@ -1,6 +1,15 @@
-exports.handler = async function(event, context) {
+exports.handler = async (event) => {
+    const queryName = event.queryStringParameters;
+    const url = `${api.geocode}access_key=${api.geoKey}&query=${query}`; 
+
+    try {
+    const response = await fetch(`${api.geocode}access_key=${api.geoKey}&query=${query}`);
     return {
-        statusCode: 200,
-        body: JSON.stringify({})
+        status: 200,
+        body: JSON.stringify(response)
+    }
+
+    } catch (error) {
+
     }
 }
